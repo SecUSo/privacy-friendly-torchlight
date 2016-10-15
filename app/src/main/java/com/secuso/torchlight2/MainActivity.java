94e95e1f25a3cf895d0d842ed3420d98bccfc766
Christopher Beckmann <chris.makas.b@gmail.com>
2016-10-15 20:07:01 +0200

    Updated to new design.

--- app/src/main/java/com/secuso/torchlight2/MainActivity.java
+++ app/src/main/java/com/secuso/torchlight2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.secuso.torchlight2;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -9,9 +14,14 @@ import android.graphics.drawable.ColorDrawable;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,7 +29,7 @@ import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends BaseActivity {
 
     private boolean flashState = false;
     private ImageButton btnSwitch;
@@ -33,15 +43,12 @@ public class MainActivity extends ActionBarActivity {
 
     private boolean isConnected;
 
+    private Activity thisActivity = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_main);
-
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#024265")));
+        setContentView(R.layout.activity_main);
 
         btnSwitch = (ImageButton) findViewById(R.id.btnSwitch);
         preferences = this.getPreferences(Context.MODE_PRIVATE);
@@ -61,6 +68,20 @@ public class MainActivity extends ActionBarActivity {
                 prefEditor.commit();
             }
         });
+
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.edit().putString("firstShow", "").commit();
+        SharedPreferences settings = getSharedPreferences("firstShow", getBaseContext().MODE_PRIVATE);
+        if (settings.getBoolean("isFirstRun", true)) {
+
+            WelcomeDialog welcomeDialog = new WelcomeDialog();
+            welcomeDialog.show(getFragmentManager(), "WelcomeDialog");
+
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("isFirstRun", false);
+            editor.commit();
+        }
 
         if(!isConnected) init();
     }
@@ -82,34 +103,78 @@ public class MainActivity extends ActionBarActivity {
             return;
         }
 
-        camera = Camera.open();
-        isConnected = true;
-        p = camera.getParameters();
+        if(ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            try {
+                if(camera == null || !isConnected || p == null) {
+                    camera = Camera.open();
+                    isConnected = true;
+                    p = camera.getParameters();
+                }
+            } catch(RuntimeException e) {
+                Toast.makeText(this, "Can not get the camera" ,Toast.LENGTH_LONG);
+            }
+        }
 
         btnSwitch.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
 
-                if (flashState) {
-                    //"info", "torch is turn off!"
-                    p.setFlashMode(Parameters.FLASH_MODE_OFF);
-                    camera.setParameters(p);
-                    camera.stopPreview();
-                    flashState = false;
-                    btnSwitch.setImageDrawable(getResources().getDrawable(R.drawable.off));
+                if (ContextCompat.checkSelfPermission(thisActivity,
+                        Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
 
-                } else {
-                    //"info", "torch is turn on!"
-                    p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-                    camera.setParameters(p);
-                    camera.startPreview();
-                    flashState = true;
-                    btnSwitch.setImageDrawable(getResources().getDrawable(R.drawable.on));
+                    ActivityCompat.requestPermissions(thisActivity, new String[]{Manifest.permission.CAMERA},0) ;
+                }
+
+                if(camera != null) {
+                    toggleCamera();
                 }
 
             }
         });
+    }
+
+    private void toggleCamera() {
+        if (flashState) {
+            //"info", "torch is turn off!"
+            p.setFlashMode(Parameters.FLASH_MODE_OFF);
+            camera.setParameters(p);
+            camera.stopPreview();
+            flashState = false;
+            btnSwitch.setImageDrawable(getResources().getDrawable(R.drawable.off));
+
+        } else {
+            //"info", "torch is turn on!"
+            p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(p);
+            camera.startPreview();
+            flashState = true;
+            btnSwitch.setImageDrawable(getResources().getDrawable(R.drawable.on));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        camera = Camera.open();
+                        isConnected = true;
+                        p = camera.getParameters();
+                        toggleCamera();
+                    } catch(RuntimeException e) {
+                        Toast.makeText(this, "An unknown error occured." ,Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Can not use flashlight without access to the camera." ,Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }
+        }
     }
 
     @Override
@@ -165,25 +230,35 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected int getNavigationDrawerID() {
+        return 0;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_about:
-                Intent intent = new Intent();
-                intent.setClass(this, AboutActivity.class);
-                if(flashState)
-                    stop();
-                else
-                    close();
-                startActivityForResult(intent, 0);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+
+    public static class WelcomeDialog extends DialogFragment {
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            LayoutInflater i = getActivity().getLayoutInflater();
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setView(i.inflate(R.layout.welcome_dialog, null));
+            builder.setIcon(R.mipmap.ic_launcher);
+            builder.setTitle(getActivity().getString(R.string.welcome));
+            builder.setPositiveButton(getActivity().getString(R.string.okay), null);
+            builder.setNegativeButton(getActivity().getString(R.string.viewhelp), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ((MainActivity)getActivity()).goToNavigationItem(R.id.nav_help);
+                }
+            });
+
+            return builder.create();
         }
     }
 }
