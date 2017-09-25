12e28c462f1f114499d6a615020fccdaa84ecd14
Amir Abdi <amirabdi.pro@gmail.com>
2017-09-25 12:06:39 +0330

    Fix issue #12

--- app/src/main/java/com/secuso/torchlight2/camera/CameraMarshmallow.java
+++ app/src/main/java/com/secuso/torchlight2/camera/CameraMarshmallow.java
@@ -22,10 +22,17 @@ public class CameraMarshmallow implements ICamera {
     public void init(Context context) {
         mContext = context;
         mCameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
-
+        
         try {
-            final String[] list = mCameraManager.getCameraIdList();
-            mCameraID = list[0];
+            for(final String cameraId : mCameraManager.getCameraIdList()){
+                CameraCharacteristics characteristics = mCameraManager.getCameraCharacteristics(cameraId);
+                int cOrientation = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if(cOrientation == CameraCharacteristics.LENS_FACING_BACK){
+                    mCameraID= cameraId;
+                }
+            }
+            
+            
         } catch (CameraAccessException e) {
             Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
@@ -36,8 +43,12 @@ public class CameraMarshmallow implements ICamera {
     @TargetApi(Build.VERSION_CODES.M)
     public boolean toggle(boolean enable) {
         try {
-            mCameraManager.setTorchMode(mCameraID, enable);
-            return true;
+            if (mCameraID != null){
+                mCameraManager.setTorchMode(mCameraID, enable);
+                return true;
+            }else{
+                 return false;
+            }
         } catch (CameraAccessException e) {
             Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
             return false;
